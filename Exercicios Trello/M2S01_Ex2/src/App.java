87ef091bc4360d35f076f373859067831d8ecd8f
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2024-02-23 19:29:23 -0300

    [M2S01] Ex 2 - Manipulando Variáveis

---
+++ Exercicios Trello/M2S01_Ex2/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        char firstLetterName = 'N';
+        float weight = 1.60f;
+        int age = 32;
+        
+        System.out.printf(
+            "Primeira letra do nome: %c\n Peso: %.2f\n Idade: %d\n", 
+            firstLetterName,
+            weight,
+            age);
+    }
+}
