9fed031c6be2dc94ef7826ba6581bba7c9bdd612
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2024-03-18 22:26:18 -0300

    [M2S05] Ex 06 - Mini-projeto - Cadastro de Heróis (Operação de Listar)

---
+++ Exercicios Trello/M2S05_Ex6/src/br/senai/devtubarao/Principal.java
@@ -0,0 +1,11 @@
+package br.senai.devtubarao;
+
+public class Principal {
+
+  public static HeroiCli heroiCli = new HeroiCli();
+  public static void main(String[] args) {
+    do {
+      heroiCli.executar();
+    } while (heroiCli.menuSelecionado != 3);
+  }
+}
