186c83b09f046864af7fccd1558cd584044da13e
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2024-04-01 19:55:24 -0300

    [M2S06] Ex 02 - Inserção de dados

---
+++ Exercicios Trello/M2S06_Ex2/Create table heroi.sql
@@ -0,0 +1,35 @@
+DO $$
+	BEGIN
+		/*
+		* DDL - Data Definition Language
+		*/
+		IF  (EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'heroi')) THEN
+			DROP TABLE heroi;
+			RAISE NOTICE 'Heroi was deleted';
+		END IF;
+			
+
+		CREATE table heroi (
+			identificador integer GENERATED ALWAYS AS IDENTITY,
+			nome varchar(255) NOT NULL,
+			superpoder varchar(255),
+			idade numeric(3),
+			data_cadastro timestamp DEFAULT current_timestamp NOT NULL
+		);
+		RAISE NOTICE 'Heroi table was created';
+
+		COMMENT ON TABLE heroi IS 'Tabela para cadastros de herois.';
+		COMMENT ON COLUMN heroi.identificador IS 'Identificador do heroi';		
+		COMMENT ON COLUMN heroi.nome IS 'Nome do heroi';
+		COMMENT ON COLUMN heroi.superpoder IS 'Superpoder do heroi';		
+		COMMENT ON COLUMN heroi.idade IS 'Idade do heroi';		
+		COMMENT ON COLUMN heroi.data_cadastro IS 'Data em que foi realizado o cadastro do heroi';
+
+		/*
+		* DML - Data Manipulation Language
+		*/
+		INSERT INTO heroi (nome, superpoder, idade) VALUES ('Spiderman', 'teia', 18);
+		INSERT INTO heroi (nome, superpoder, idade) VALUES ('Iron Man', 'armadura', 40);
+		INSERT INTO heroi (nome, superpoder, idade) VALUES ('Batman', 'bat-coisas', 39);
+		INSERT INTO heroi (nome, superpoder, idade) VALUES ('Superman', 'super-força', 26);
+END $$;
---
+++ Exercicios Trello/M2S06_Ex2/heroi.sql
@@ -0,0 +1,4 @@
+INSERT INTO heroi (nome, superpoder, idade) VALUES ('Spiderman', 'teia', 18);
+INSERT INTO heroi (nome, superpoder, idade) VALUES ('Iron Man', 'armadura', 40);
+INSERT INTO heroi (nome, superpoder, idade) VALUES ('Batman', 'bat-coisas', 39);
+INSERT INTO heroi (nome, superpoder, idade) VALUES ('Superman', 'super-força', 26);
