7eab210589a990066215006cc205bb7246683b3e
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2024-04-01 19:49:12 -0300

    [M2S06] Ex 01 - Tabela de Heróis

---
+++ Exercicios Trello/M2S06_Ex1/heroi.sql
@@ -0,0 +1,15 @@
+
+CREATE table heroi (
+	identificador integer GENERATED ALWAYS AS IDENTITY,
+	nome varchar(255) NOT NULL,
+	superpoder varchar(255),
+	idade numeric(3),
+	data_cadastro timestamp DEFAULT current_timestamp NOT NULL
+);
+
+COMMENT ON TABLE heroi IS 'Tabela para cadastros de herois.';
+COMMENT ON COLUMN heroi.identificador IS 'Identificador do heroi';		
+COMMENT ON COLUMN heroi.nome IS 'Nome do heroi';
+COMMENT ON COLUMN heroi.superpoder IS 'Superpoder do heroi';		
+COMMENT ON COLUMN heroi.idade IS 'Idade do heroi';		
+COMMENT ON COLUMN heroi.data_cadastro IS 'Data em que foi realizado o cadastro do heroi';
